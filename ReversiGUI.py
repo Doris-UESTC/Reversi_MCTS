ad9630be85a0f007f4dbc47177ec828655473825
free-stardust <gblvso@163.com>
2022-09-25 21:35:45 +0800

    emmmm

--- ReversiGUI.py
+++ ReversiGUI.py
@@ -262,6 +262,8 @@ class ReversiGUI(Frame):
 
     def moveNext(self, event):
         """下棋"""
+        if self.playMode == PlayMode.AIVSAI:
+            return
         if self.judgePlayMode() == False:
             return
         if self.game_over() == True:
